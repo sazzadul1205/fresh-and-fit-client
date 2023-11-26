691a4774f073f4fb7a369bc46748b126eb579d10
sazzadul1205 <psazzadul@gmail.com>
2023-11-26 13:03:20 +0600

    add classes page and a few bug fixes

--- src/Pages/AddClasses/AddClasses.jsx
+++ src/Pages/AddClasses/AddClasses.jsx
@@ -1,13 +1,55 @@
 import { motion } from "framer-motion";
 import { useForm } from "react-hook-form";
 import Title from "../Shared/PageTitles/Title";
+import useAuth from "../../Hooks/useAuth";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AddClasses = () => {
     const { register, handleSubmit } = useForm();
+    const { user } = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
 
     const onSubmit = (data) => {
-        console.log(data);
-    }
+        const classInfo = {
+            className: data.className,
+            classDescription: data.description,
+            trainerName: user.displayName,
+            trainerEmail: user.email,
+            classTime: `${data.classStart} - ${data.classEnd}`,
+        };
+
+        axiosPublic
+            .post('/classes', classInfo)
+            .then((res) => {
+                if (res.data.insertedId) {
+                    showSuccessAlert();
+                    navigate('/classes');
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                showErrorAlert('Failed to create a new class.');
+            });
+    };
+
+    const showSuccessAlert = () => {
+        Swal.fire({
+            icon: 'success',
+            title: 'Class Created!',
+            text: 'Your fitness class has been successfully created.',
+        });
+    };
+
+    const showErrorAlert = (errorMessage) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Class Creation Failed',
+            text: errorMessage,
+        });
+    };
 
     return (
         <div>
@@ -18,7 +60,7 @@ const AddClasses = () => {
                 ></Title>
             </div>
             <div>
-                <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100 opacity-90">
+                <div className="card md:w-1/2 max-w-sm shadow-2xl bg-red-500 opacity-90 mx-auto">
                     <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                         <div className="form-control">
                             <label className="label">
@@ -39,6 +81,7 @@ const AddClasses = () => {
                             <input
                                 type="text"
                                 {...register('trainerName')}
+                                defaultValue={user.displayName}
                                 name="trainerName"
                                 placeholder="Trainer's Name"
                                 className="input input-bordered"
@@ -82,7 +125,7 @@ const AddClasses = () => {
                         </div>
                         <div className="form-control mt-6">
                             <motion.input
-                                className={`w-full p-3 bg-red-500 hover:bg-red-800 disabled:bg-gray-500 rounded-xl`}
+                                className={`w-full p-3 bg-purple-500 hover:bg-red-800 disabled:bg-gray-500 rounded-xl`}
                                 type='submit'
                                 value={'Create a new class'}
                                 whileHover={{ scale: 1.2 }}
